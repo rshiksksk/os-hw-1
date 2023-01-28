b07da69355ab99b5d9030c04f7646c5b66579b60
rdonthula <rishiboyrj5@gmail.com>
2023-01-28 02:25:25 -0500

    first draft of soln2, has issue

--- sed.c
+++ sed.c
@@ -1,1 +1,57 @@
-soln2
+#include "user.h"
+
+void sed(int fd, char* old, char* new);
+
+int is_matching(char* old, char* curr);
+
+int main(int argc, char *argv[])
+{
+
+
+    if (argc < 3 || argc > 4){
+        //i.e. only sed, first word, and second word are given OR
+        //sed, first word, second word, filename and something else is given
+        printf(1, "wrong number (%d)of cmd line arguments", argc);
+        exit();
+    }
+
+    int fd;
+    if (argc == 3){
+        fd = 0;
+    }
+    else {fd = open(argv[3], 0);}
+
+    char* old = argv[1];
+    char* new = argv[2];
+
+    sed(fd, old, new);
+    close(fd);
+    exit();
+}
+
+
+void sed(int fd, char* old, char* new) {
+
+
+    int len = strlen(old);
+    char ch[len];
+    int n;
+    while ((n = read(0, ch, len))>0){
+        if (!is_matching(old, ch)){
+            write(fd, ch, len);
+        }
+        else {
+            write(fd, new, strlen(new));
+        }
+    }
+}
+
+int is_matching(char* old, char* curr) {
+    int i;
+    for (i = 0; i<strlen(old);i++){
+        if (old[i] != curr[i]) {
+            return 0;
+        }
+    }
+    return 1;
+};
